eb7f36d6b70060ef8e7213fecaa7ef037652d309
凡梦星尘 <elkan1788@gmail.com>
2018-02-23 18:17:57 +0800

    init project

---
+++ bin/email_job.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# __author__ = 'elkan1788@gmail.com'
+
+from emailjob.main import run
+
+import sys
+
+if __name__ == '__main__':
+    run(sys.argv)
---
+++ setup.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+# __author = 'elkan1788@gmail.com'
+
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+INSTALL_REQ = parse_requirements('requirements.txt', session='hack')
+REQUIRES = [str(ir.req) for ir in INSTALL_REQ]
+
+"""
+Email Job setup requires 
+"""
+setup(
+    name='email_job',
+    version='2.0.4',
+    description='Use Python implements get data from Hive then send attachments to user.',
+    author='elkan1788',
+    author_email='elkan1788@gmail.com',
+    license='MIT',
+    install_requires=REQUIRES,
+    packages=find_packages()
+)
